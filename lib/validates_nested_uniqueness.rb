17ae2be95637ae9d4987a6a09e175966327a956d
Anton Maminov <anton.linux@gmail.com>
2022-07-27 12:00:20 +0300

    Prefer keyword arguments for arguments with a boolean default value

--- lib/validates_nested_uniqueness.rb
+++ lib/validates_nested_uniqueness.rb
@@ -28,7 +28,8 @@ module ActiveRecord
         indexed_attribute = reflection.options[:index_errors] || ActiveRecord::Base.try(:index_nested_attribute_errors)
 
         value.reject(&:marked_for_destruction?).map.with_index do |nested_value, index|
-          normalized_attribute = normalize_attribute(association_name, indexed_attribute, index)
+          normalized_attribute = normalize_attribute(association_name, indexed_attribute: indexed_attribute,
+                                                                       index: index)
 
           track_value = @scope.each.each_with_object({}) do |(k), memo|
             memo[k] = nested_value.try(k)
@@ -61,7 +62,7 @@ module ActiveRecord
 
       private
 
-      def normalize_attribute(association_name, indexed_attribute = false, index = nil)
+      def normalize_attribute(association_name, indexed_attribute: false, index: nil)
         if indexed_attribute
           "#{association_name}[#{index}].#{@attribute_name}"
         else
